043b3711c2fe56e3896b67b3e8ae897afac9a094
Vasil Diadov <vasil.diadov@softmachines.com>
2017-06-06 16:19:33 +0300

    New functionality in Predicates.

--- src/predicates.ml
+++ src/predicates.ml
@@ -1,7 +1,7 @@
 type t =
   { mutable v : bool;
     mutable pv : bool;
-    eval : unit -> unit}
+    mutable eval : unit -> unit}
 
 let make f =
   let rec r =
@@ -12,6 +12,53 @@ let make f =
                 r.v <- f ())} in
   r
 
-let (!?.) r = r.eval (); r.v
-let (?.) r = r.v
-let (??.) r = r.pv
+let eval_list p_list =
+  List.iter (fun p -> p.eval ()) p_list
+
+let on_eval p p_list =
+  let old_eval = p.eval in
+  p.eval <-
+    fun () -> 
+      old_eval (); eval_list p_list
+
+let sync_eval p_list =
+  let old_eval = List.map (fun p -> p.eval) p_list in
+  let eval = fun () ->
+    List.iter (fun e -> e ()) old_eval in
+  List.iter (fun p -> p.eval <- eval) p_list
+
+let ( && ) p1 p2 =
+  make
+    (fun () ->
+       p1.eval();
+       if p1.v
+       then (p2.eval(); p2.v)
+       else false)
+
+let ( || ) p1 p2 =
+  make
+    (fun () ->
+       p1.eval();
+       if not p1.v
+       then (p2.eval(); p2.v)
+       else true)
+
+let not p =
+  make
+    (fun () ->
+       p.eval();
+       not p.v)
+
+let history ?(depth=10) p =
+ let h = Array.make depth false in
+ let i = ref 0 in
+ let old_eval = p.eval in
+ let get_hist n =
+   let j = if !i < n + 1 then !i - n - 1 + depth else !i - n - 1 in
+   Array.get h j in
+ let eval () =
+   old_eval ();
+   Array.set h !i p.v;
+   i := (!i + 1) mod depth; in
+ p.eval <- eval;
+ get_hist
--- src/predicates.mli
+++ src/predicates.mli
@@ -1,5 +1,13 @@
-type t = { mutable v : bool; mutable pv : bool; eval : unit -> unit }
+type t = {
+  mutable v : bool;
+  mutable pv : bool;
+  mutable eval : unit -> unit;
+}
 val make : (unit -> bool) -> t
-val ( !?. ) : t -> bool
-val ( ?. ) : t -> bool
-val ( ??. ) : t -> bool
+val eval_list : t list -> unit
+val on_eval : t -> t list -> unit
+val sync_eval : t list -> unit
+val ( && ) : t -> t -> t
+val ( || ) : t -> t -> t
+val not : t -> t
+val history : ?depth:int -> t -> int -> bool
